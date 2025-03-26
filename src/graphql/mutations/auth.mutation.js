fc652c3248e1bd4e9cf3e02ccb91960654ac8bb0
Wisnu Box <hwisnu222@gmail.com>
2025-03-26 21:36:41 +0700

    feat: add mutation for login

---
+++ src/graphql/mutations/auth.mutation.js
@@ -0,0 +1,11 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN = gql`
+  mutation TokenAuth($username: String!, $password: String!) {
+    tokenAuth(username: $username, password: $password) {
+      token
+      payload
+      refreshExpiresIn
+    }
+  }
+`;
